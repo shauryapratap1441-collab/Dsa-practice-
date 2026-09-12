b3ac6daaacfb56fa68521c50655e41c398f02813
Shaurya Pratap Singh <shauryapratap1441@gmail.com>
2026-09-12 15:05:33 +0530

    Time: 2 ms (94.95%), Space: 47.8 MB (24.64%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,24 +1,27 @@
 import java.util.*;
+
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        Map<Integer,Integer> map=new HashMap<>();
-        Set<Integer> set=new HashSet<>();
-        for(int num:nums){
-        map.put(num,map.getOrDefault(num,0)+1);
-        set.add(num);
+        int n = nums.length;
+        int[] counts = new int[n + 1];
+        
+        // Count frequencies of each number
+        for (int num : nums) {
+            counts[num]++;
         }
-        int duplicate=0;
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            if(entry.getValue()>1)
-            {
-                duplicate = entry.getKey();
-                break;
+        
+        int duplicate = 0;
+        int missing = 0;
+        
+        // Look for the duplicate (count == 2) and missing (count == 0)
+        for (int i = 1; i <= n; i++) {
+            if (counts[i] == 2) {
+                duplicate = i;
+            } else if (counts[i] == 0) {
+                missing = i;
             }
         }
-        int missing=1;
-        while(set.contains(missing)){
-            missing++;
-        }
-        return new int[]{duplicate,missing};
+        
+        return new int[]{duplicate, missing};
     }
 }
